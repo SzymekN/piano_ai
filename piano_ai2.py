0502624b29f0662ae31b2d6ac0302b4071fde74b
SzymekN <nowak.szymek00@gmail.com>
2021-10-08 12:12:30 +0200

    faster algorithm

--- piano_ai2.py
+++ piano_ai2.py
@@ -6,35 +6,38 @@ import win32api
 import win32con
 from ctypes import windll
 
-
-pos_1 = (970,500)
-pos_2 = (1130,500)
-pos_3 = (1300,500)
-pos_4 = (1455,500)
-
-
-def is_black(tile):
-    if tile_1[0] <50 and tile[1] < 50 and tile[2] < 50:
-        return True
-    return False
-
 def click(pos):
+    # click at given position
     win32api.SetCursorPos((pos[0],pos[1]))
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0)
-    time.sleep(0.01) #This pauses the script for 0.1 seconds
+    time.sleep(0.001) #This pauses the script for 0.1 seconds
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)
 
 def calibration():
+    # calibrete where are tiles 
+
+    # get state of left mb
     state_left = win32api.GetKeyState(0x01)
     while True:
+        # if left mb state change return mouse position
         clicked = win32api.GetKeyState(0x01)
         if clicked != state_left:
             time.sleep(0.1)
             return pyautogui.position()
 
 def get_pixel(pos):
+    # get pixel value
     return windll.gdi32.GetPixel(dc,pos[0],pos[1])
 
+
+
+# default tiles positions
+pos_1 = (970,500)
+pos_2 = (1130,500)
+pos_3 = (1300,500)
+pos_4 = (1455,500)
+
+# calibrate tiles position or use defaults
 choice = input("Calibrate?")
 if choice == 'y':
     print("Click on tile 1")
@@ -47,7 +50,7 @@ if choice == 'y':
     print("Click on tile 4")
     pos_4 = calibration()
     print("Calibrated")
-
+    
 
 dc = windll.user32.GetDC(0)
 
@@ -65,39 +68,33 @@ while keyboard.is_pressed('q') == False:
 
         while keyboard.is_pressed('q') == False: 
             start = time.time()
-            img = pyautogui.screenshot()
 
             # niefortunnie za wolno
-            # tile_1 = get_pixel(pos_1)
-            # tile_2 = get_pixel(pos_2)
-            # tile_3 = get_pixel(pos_3)
-            # tile_4 = get_pixel(pos_4)
-
-            # color_1 = int(bin(tile_1)[-8:], 2) 
-            # color_2 = int(bin(tile_2)[-8:], 2) 
-            # color_3 = int(bin(tile_3)[-8:], 2) 
-            # color_4 = int(bin(tile_4)[-8:], 2) 
+            tile_1 = get_pixel(pos_1)
+            color_1 = int(bin(tile_1)[-8:], 2) 
+            if color_1 <20:
+                click(pos_1)
+                # print("1")
+            else:
+                tile_2 = get_pixel(pos_2)
+                color_2 = int(bin(tile_2)[-8:], 2) 
+                if color_2 <20:
+                    click(pos_2)
+                    # print("2")
+                else:
+                    tile_3 = get_pixel(pos_3)
+                    color_3 = int(bin(tile_3)[-8:], 2) 
+                    if color_3 <20:
+                        click(pos_3)
+                        # print("3")
+                    else:
+                        tile_4 = get_pixel(pos_4)
+                        color_4 = int(bin(tile_4)[-8:], 2) 
+                        if color_4 <20:
+                            click(pos_4)
+                            # print("4")
+                        # else:
+                            # print("none")
 
             end = time.time()
             print(end-start)
-
-            tile_1 = img.getpixel(pos_1)
-            tile_2 = img.getpixel(pos_2)
-            tile_3 = img.getpixel(pos_3)
-            tile_4 = img.getpixel(pos_4)
-
-            if tile_1[0] <20:
-                click(pos_1)
-            if tile_2[0] <20:
-                click(pos_2)
-            if tile_3[0] <20:
-                click(pos_3)
-            if tile_4[0] <20:
-                click(pos_4)
-                
-        
-print(tile_1)
-
-#x 50,160,270,360
-#y 500
-
